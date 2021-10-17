1be1077371be5df7151f8b8b7c13c0f470a7ae71
Joseph Bailey <josephlbailey@hey.com>
2021-10-17 15:12:37 -0400

    Add finished project

---
+++ README.md
@@ -0,0 +1,45 @@
+# What is this project? 
+
+US Mobile offers "Perk" discounts for customers using autopay. These perks include free subcriptions to streaming services. I have multiple family members on my plan, so I qualify for 2 "free" streaming subscriptions. The drawback to this is that US Mobile "pays" for these subscriptions via a discount on your plan. They require a screenshot of each receipt each month emailed to their validation mailbox. Enter this project. I figured why waste 5 minutes of my time each month when I can spend 15 hours writing an extremely brittle, rigid, and poorly coded project to automatically grab a screenshot of each receipt and email it to US Mobile.
+
+This project currently only supports Spotify __AND__ Disney Plus, as those are the two streaming services I use. Perhaps in the future when I have another peak in the waves of my motivation, I'll add support for the other services. Oh yeah, this project requires that you have an outlook mailbox to send these emails from. I think you'll have to have that mailbox set up as the primary email on your US Mobile account as well for them to accept the screenshots and apply them to your account.
+
+You'll have to provide a [dotenv](https://nodejs.dev/learn/how-to-read-environment-variables-from-nodejs) file in the project root with your creds. That file will have to look like this: 
+
+``` bash
+# spotify
+SPTFY_UID="your_spotify_uid"
+SPTFY_PWD="your_spotify_password"
+
+# disney
+DSNY_UID="your_disney_uid"
+DSNY_PWD="your_disney_password"
+
+# outlook
+OUTLK_UID="you@outlook.com"
+OUTLK_PWD="your_outlook_password"
+
+# "from" name that goes into nodemailer and email template
+OUTLK_FRM="Your Name"
+
+# numbers that go in the email template
+NUMS="123-456-7890,234-567-8901"
+```
+
+Also, this uses MS Edge headless, but you can remove the `executablePath` property in the puppeteer launch config if you want to use chromium.
+
+This project uses the userAgent package to help with logging into Spotify.
+
+## How to run
+1. Make sure you have your `.env` file in the root of the project. 
+2. Run the npm script: `npm run mainTask`
+
+You can also do a dry-run to make sure everything works properly without actually sending the email. This will print out the nodemailer transport options and the mail options that have been configured. You can do this by passing the `--dry-run` option like this: 
+
+`npm run mainTask -- --dry-run`
+
+Note: no-one in their right mind should use this unless you have the *exact* scenario I have: 
+- At least 4 lines with US Mobile on autopay
+- Have Spotify Premium Family
+- Have Disney Plus (Hulu, ESPN, Disney bundle plan thingy)
+- Have a desire to waste your time
---
+++ index.js
@@ -0,0 +1,200 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const userAgent = require('user-agents');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+let spotifyScreenshotPath = '';
+let disneyPlusScreenshotPath = '';
+let spotifyScreenshotFilename = '';
+let disneyPlusScreenshotFilename = '';
+
+let spotifyUid = process.env.SPTFY_UID;
+let spotifyPwd = process.env.SPTFY_PWD;
+let disneyUid = process.env.DSNY_UID;
+let disneyPwd = process.env.DSNY_PWD;
+let outlookUid = process.env.OUTLK_UID;
+let outlookPwd = process.env.OUTLK_PWD;
+let outlookFrm = process.env.OUTLK_FRM;
+let nums = process.env.NUMS;
+let num1 = nums.split(',')[0];
+let num2 = nums.split(',')[1];
+let dryRun = false;
+
+function setUp() {
+    dryRun = process.argv.indexOf('--dry-run') !== -1;
+
+    fs.mkdir('screenshots', (err) => {
+        if (err) {
+            if (err.code === 'EEXIST') {
+                return;
+            }
+
+            throw err;
+        }
+    });
+}
+
+
+async function getSpotifyReceipt() {
+    const browser = await puppeteer.launch({
+        executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe'
+    });
+    const page = await browser.newPage();
+    await page.setUserAgent(userAgent.toString());
+    page.setViewport({ width: 1200, height: 1000 });
+    await page.goto('https://accounts.spotify.com/en/login/?_locale=en-US&continue=https:%2F%2Fwww.spotify.com%2Fus%2Faccount%2Fsubscription%2Freceipt%2F');
+    await page.waitForSelector('#login-button');
+    await page.type('#login-username', spotifyUid);
+    await page.type('#login-password', spotifyPwd);
+    await page.click('#login-button');
+    await page.waitForSelector('#table-receipts > tbody > tr:nth-child(1)');
+    await page.click('#table-receipts > tbody > tr:nth-child(1) > td.receipt-action > a');
+    await page.waitForSelector('body > div.wrap > div.outer-content-wrapper > div.content-wrapper > div > div > div.col-sm-9 > div > div');
+    const receipt = await page.$('body > div.wrap > div.outer-content-wrapper > div.content-wrapper > div > div > div.col-sm-9 > div > div');
+
+    spotifyScreenshotFilename = `spotify_screenshot_${Math.floor(Date.now() / 1000)}.png`;
+    spotifyScreenshotPath = `./screenshots/${spotifyScreenshotFilename}`;
+
+    await receipt.screenshot({ path: spotifyScreenshotPath });
+
+    await browser.close();
+}
+
+async function getDisneyReceipt() {
+    const browser = await puppeteer.launch({
+        executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe'
+    });
+    const page = await browser.newPage();
+    await page.setUserAgent(userAgent.toString());
+    page.setViewport({ width: 1200, height: 1000 });
+    await page.goto('https://disneyplus.com/login');
+    await page.waitForSelector('#email');
+    await page.type('#email', disneyUid);
+    await page.click('#dssLogin > div:nth-child(3) > button');
+    await page.waitForSelector('#password');
+    await page.type('#password', disneyPwd);
+    await page.click('#dssLogin > div > button');
+    await page.waitForSelector('#remove-main-padding_index > div > div > section > ul > div:nth-child(1) > div > div');
+    await page.click('#remove-main-padding_index > div > div > section > ul > div:nth-child(1) > div > div');
+    await page.waitForSelector('#active-profile > div > div');
+    await page.hover('#active-profile > div > div');
+    await page.click('#dropdown-option_account > a');
+    await page.waitForSelector('[data-testid="billing-history-link"]');
+    await page.click('[data-testid="billing-history-link"]');
+    await page.waitForSelector('[data-testid*="invoice-link-"]');
+    await page.click('[data-testid*="invoice-link-"]');
+    await page.waitForSelector('[data-gv2containerkey="modalContainer"] > div');
+    var receipt = await page.$('[data-gv2containerkey="modalContainer"] > div');
+    disneyPlusScreenshotFilename = `disney_plus_screenshot_${Math.floor(Date.now() / 1000)}.png`;
+    disneyPlusScreenshotPath = `./screenshots/${disneyPlusScreenshotFilename}`;
+    await receipt.screenshot({ path: disneyPlusScreenshotPath });
+    await browser.close();
+}
+
+async function main() {
+    try {
+        setUp();
+        await getDisneyReceipt();
+        await getSpotifyReceipt();
+    }
+    catch (error) {
+        await handleError(error);
+        throw error;
+    }
+
+    let emailBody = fs.readFileSync('email_body.html').toString();
+    emailBody = emailBody.replace('[[NUM1]]', num1);
+    emailBody = emailBody.replace('[[NUM2]]', num2);
+    emailBody = emailBody.replace('[[OUTLK_FRM]]', outlookFrm);
+
+    var transporter = nodemailer.createTransport({
+        host: 'smtp-mail.outlook.com',
+        secure: false,
+        port: 587,
+        tls: {
+            ciphers: 'SSLv3'
+        },
+        auth: {
+            user: outlookUid,
+            pass: outlookPwd
+        }
+    });
+
+    var mailOptions = {
+        from: `"${outlookFrm}" <${outlookUid}>`,
+        to: 'verification@usmobile.com',
+        bcc: [
+            outlookUid
+        ],
+        subject: 'Disney Plus and Spotify Premium perk verification',
+        html: emailBody,
+        attachments: [
+            {
+                filename: spotifyScreenshotFilename,
+                content: fs.createReadStream(spotifyScreenshotPath)
+            },
+            {
+                filename: disneyPlusScreenshotFilename,
+                content: fs.createReadStream(disneyPlusScreenshotPath)
+            }
+        ]
+    }
+
+    if (!dryRun) {
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                return console.log(error);
+            }
+
+            console.log(`Message sent: ${info.response}`);
+        });
+    }
+    else {
+        console.log('********************* Dry Run *******************');
+        console.log('Here\'s what would have been sent: ');
+        console.log('transport options: ');
+        transporter.options.auth.pass = '********************'
+        console.log(transporter.options);
+        console.log('mailOptions');
+        console.log(mailOptions);
+    }
+}
+
+async function handleError(error) {
+
+    var transporter = nodemailer.createTransport({
+        host: 'smtp-mail.outlook.com',
+        secure: false,
+        port: 587,
+        tls: {
+            ciphers: 'SSLv3'
+        },
+        auth: {
+            user: outlookUid,
+            pass: outlookPwd
+        }
+    });
+
+    var mailOptions = {
+        from: `"${outlookFrm}" <${outlookUid}>`,
+        to: outlookUid,
+        subject: 'US Mobile verification failure',
+        html: `Error getting receipts for verification. Error: ${error}`
+    }
+
+    console.log(transporter.options);
+
+    if (!dryRun) {
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                return console.log(error);
+            }
+
+            console.log(`Message sent: ${info.response}`);
+        });
+    }
+}
+
+main().catch(console.error);
